929d95c9b634b6be3ed2d522357dcc7ac322cb8a
Jenia <podophobia@gmail.com>
2014-11-21 21:41:01 +0200

    add strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-
+   
 
     def signup
     if params[:fb_uid].present?
@@ -22,76 +22,81 @@ class UsersController < ApplicationController
   end
 
 
+  
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+ 
 
 
-#  # GET /users
-#  # GET /users.json
-#  def index
-#    @users = User.all
-#  end
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+       format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
-#  # GET /users/1
-#  # GET /users/1.json
-#  def show
-#  end
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
-#  # GET /users/new
-#  def new
-#    @user = User.new
-#  end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
 
-#  # GET /users/1/edit
-#  def edit
-#  end
-
-#  # POST /users
-#  # POST /users.json
-#  def create
-#    @user = User.new(user_params)
-
-#    respond_to do |format|
-#      if @user.save
-#        format.html { redirect_to @user, notice: 'User was successfully created.' }
-#        format.json { render :show, status: :created, location: @user }
-#      else
-#        format.html { render :new }
-#        format.json { render json: @user.errors, status: :unprocessable_entity }
-#      end
-#    end
-#  end
-
-#  # PATCH/PUT /users/1
-#  # PATCH/PUT /users/1.json
-#  def update
-#    respond_to do |format|
-#      if @user.update(user_params)
-#        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-#        format.json { render :show, status: :ok, location: @user }
-#      else
-#       format.html { render :edit }
-#        format.json { render json: @user.errors, status: :unprocessable_entity }
-#      end
-#    end
-#  end
-
-#  # DELETE /users/1
-#  # DELETE /users/1.json
-#  def destroy
-#    @user.destroy
-#    respond_to do |format|
-#      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-#      format.json { head :no_content }
-#    end
-#  end
-
-#  private
-#    # Use callbacks to share common setup or constraints between actions.
-#    def set_user
-#      @user = User.find(params[:id])
-#    end
-
-#    # Never trust parameters from the scary internet, only allow the white list through.
-#    def user_params
-#      params.require(:user).permit(:username, :email, :password, :unique_id, :name, :about)
-#    end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :unique_id, :name, :about)
+    end
 end
--- app/models/baby.rb
+++ app/models/baby.rb
@@ -1,3 +1,4 @@
 class Baby < ActiveRecord::Base
+  
   belongs_to :user
 end
