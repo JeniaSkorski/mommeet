c70790c0a557dec74520279a063ed61b482fdcd0
Jenia <podophobia@gmail.com>
2014-11-22 13:44:13 +0200

    make url for post request

--- config/routes.rb
+++ config/routes.rb
@@ -15,13 +15,13 @@ Rails.application.routes.draw do
   
   resources :babies
 
-  resources :users 
-  
-  
+  resources :users do
+    collection do
+         post 'new'
+    end
+  end
   
 
-  
-  
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
