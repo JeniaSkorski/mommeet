818573e7e8f6e2c278b4d3a21e29e5f34c924cdc
Jenia <podophobia@gmail.com>
2014-12-17 11:41:11 +0200

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,24 +15,24 @@ Rails.application.routes.draw do
   
   resources :babies
 
-  #resources :users do
-   # collection do
-    #     post 'new'
-     #    post 'create'
+  resources :users do
+    collection do
+         post 'new'
+         post 'create'
+    end
+  end
+  
+  #Rails.application.routes.draw do
+  #namespace :api, :defaults => {:format => :json} do
+   #namespace :v1 do
+   #   get     "/users",     to: "users#index"
+    #  post    "/users",     to: "users#create"
+     # get     "/users/:id", to: "users#show"
+     # put     "/users/:id", to: "users#update"
+     # delete  "/users/:id", to: "users#destroy"
     #end
   #end
-  
-  Rails.application.routes.draw do
-  namespace :api, :defaults => {:format => :json} do
-   #namespace :v1 do
-      get     "/users",     to: "users#index"
-      post    "/users",     to: "users#create"
-      get     "/users/:id", to: "users#show"
-      put     "/users/:id", to: "users#update"
-      delete  "/users/:id", to: "users#destroy"
-    #end
-  end
-end
+#end
   
   
   
